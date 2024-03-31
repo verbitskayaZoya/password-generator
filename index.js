99b4f1d7df5bf53db397eaf0e4e5c9de086efc0a
verbitskayaZoya <103507783+verbitskayaZoya@users.noreply.github.com>
2024-03-31 19:45:16 +0700

    Refactored js
    Made code drier

--- index.js
+++ index.js
@@ -1,140 +1,106 @@
 import { characters, charactersWithoutNumbers, 
     charactersWithoutSymbols, onlyLetters } from "./characters.js"
     
-    // ---------- GETTING ELEMENTS -------------------------------
-    const passwordContainer = document.getElementById("password-container")
-    const firstPassword = document.getElementById("first-password")
-    const secondPassword = document.getElementById("second-password")
-    const button = document.getElementById("btn-password")
-    const btnContainer = document.getElementById('btn-container')
-    const errorMessage  = document.getElementById("error-message")
-    const passwordLengthInput = document.getElementById("password-length-input")
-    const noNumsBtn = document.getElementById('no-numbers-btn')
-    const noSymbBtn = document.getElementById("no-symbols-btn")
-    const toggleBtn = document.getElementById("toggle-button")
-    const pageContainer = document.getElementById("page-container")
+// ---------- GETTING ELEMENTS -------------------------------
+    const passwordContainer     = document.getElementById("password-container")
+    const btnContainer          = document.getElementById('btn-container')
+    const pageContainer         = document.getElementById("page-container")
+    const firstPassword         = document.getElementById("first-password")
+    const secondPassword        = document.getElementById("second-password")
+    const btnGeneratePassword   = document.getElementById("btn-generate-password")
+    const errorMessage          = document.getElementById("error-message")
+    const passwordLengthInput   = document.getElementById("password-length-input")
+    const toggleBtn             = document.getElementById("toggle-button")
     const passwordCopiedMessage = document.getElementById("password-copied-message")
     
-    //--------- GLOBAL VARIABLES -----------------------------------
+//--------- GLOBAL VARIABLES -----------------------------------
     let password = " "
-
-
-
-    let isNoNumsClicked = false
-    let isNoSymbClicked = false
+    let btnState = {}
     let hasDarkTheme = true
     
-    //-------- EVENT LISTENERS ---------------------------------------------
-    // NO NUMS BTN STATE AND STYLE ON CLICK
-    // noNumsBtn.addEventListener("click", () => {
-    //     isNoNumsClicked ? (isNoNumsClicked  = false,
-    //         noNumsBtn.style.backgroundColor = "D5D4D8") // Is there a way to use a CSS variable here instead?
-    //         : (isNoNumsClicked = true,
-    //         noNumsBtn.style.backgroundColor = "#10B981")
-    // })
+//-------- EVENT LISTENERS ---------------------------------------------
     
-
-
-    btnContainer.addEventListener("click", (e) => changeBntStyle(e.target))
-
-   function changeBntStyle(btn) {
-    console.log(btn.id)
-        if(btn.id === "no-numbers-btn") {
-            isNoNumsClicked ? (isNoNumsClicked  = false,
-                btn.style.backgroundColor = "D5D4D8") 
-                : (isNoNumsClicked = true,
-                btn.style.backgroundColor = "#10B981")
-        }
-   }
-    // NO SYMB BTN STATE AND STYLE ON CLICK
-    noSymbBtn.addEventListener("click", ()  => {
-        isNoSymbClicked ? (isNoSymbClicked  = false,
-                        noSymbBtn.style.backgroundColor = "D5D4D8") // Is there a way to use a CSS variable here instead?
-                        : (isNoSymbClicked = true,
-                        noSymbBtn.style.backgroundColor = "#10B981")
-    })
-    
-     //---- CHANGE THEME -------------------------------------------------
-    toggleBtn.addEventListener("click", () => {
-        hasDarkTheme ? (hasDarkTheme = false,
-            pageContainer.classList.add("light-theme"),
-            pageContainer.classList.remove("dark-theme") )
-            : (hasDarkTheme = true,
-            pageContainer.classList.remove("light-theme"),
-            pageContainer.classList.add("dark-theme"))
-        }
-      
-    )
-
-
-    // COPY TO CLIPBOARD
-    function copyToClipboard(element) {
-        navigator.clipboard.writeText(element.textContent)
-        element.textContent ? passwordCopiedMessage.textContent = `Copied`
-            : '' 
-            setTimeout(() => {
-                passwordCopiedMessage.textContent = ''
-            }, 1000);
-    }
-    
-    // GENERATE PASSWORDS BTN
-    button.addEventListener("click", renderPasswords)
+    btnContainer.addEventListener("click", (e) => changeBtnColor(e.target, e.target.id))
+    btnGeneratePassword.addEventListener("click", renderPasswords)
     passwordContainer.addEventListener("click", (e) => copyToClipboard(e.target))
+    toggleBtn.addEventListener("click", changeTheme)
     
-    // ------------------- FUNCTIONS DECLARATIONS ------------------------------
-    
-    function renderPasswords() {
-        firstPassword.textContent = generatePasswords(passwordLengthInput.value)
-        secondPassword.textContent = generatePasswords(passwordLengthInput.value)
+// ------------------- FUNCTIONS DECLARATIONS ------------------------------    
+
+//---- CHANGE DARK/LIGHT THEME -------------------------------------------------
+function changeTheme() {
+    hasDarkTheme ? (hasDarkTheme = false,
+        pageContainer.classList.add("light-theme"),
+        pageContainer.classList.remove("dark-theme") )
+        : (hasDarkTheme = true,
+        pageContainer.classList.remove("light-theme"),
+        pageContainer.classList.add("dark-theme"))
+    }    
+
+//----- CHANGE STYLE OF BUTTONS (CLICKED/NOT CLICKED) AS TOGGLES ON/OFF ----------
+function changeBtnColor(btn, stateName) {
+    btnState[stateName] ? (btnState[stateName]  = false,
+        btn.style.backgroundColor = "#10B981") // default btn color (mountainmeadow in css)
+        : (btnState[stateName] = true,
+        btn.style.backgroundColor = "#6B7280") // slateGray color
     }
+
+// ----- COPY TO CLIPBOARD -----------------------------------------------------
+function copyToClipboard(element) {
+    navigator.clipboard.writeText(element.textContent)
+    element.textContent ? passwordCopiedMessage.textContent = `Copied`
+                        : '' 
+    setTimeout(() => {
+        passwordCopiedMessage.textContent = ''
+    }, 1000);
+}
+
+function renderPasswords() {
+    firstPassword.textContent = generatePasswords(passwordLengthInput.value)
+    secondPassword.textContent = generatePasswords(passwordLengthInput.value)
+}
     
-    function generatePasswords(num) {
-        password = " "
-        errorMessage.textContent = ''
+function generatePasswords(num) {
+    password = " "
+    errorMessage.textContent = ''
         try{
             if(!num) throw "it's empty"
             if(isNaN(num)) throw "it's not a number"
             if(num > 15) throw "it's more than fifteen"
             if(num < 5) throw "it's less than five"
-        
-        if(isNoNumsClicked && isNoSymbClicked) {
-            for(let i = 0; i < num; i++) {
-                let randomCharacterIndex = Math.floor(Math.random() * onlyLetters.length)     
-                password += onlyLetters[randomCharacterIndex]
-                } 
-            return password
+
+            if(btnState.numbers && btnState.symbols) {
+                for(let i = 0; i < num; i++) {
+                    let randomCharacterIndex = Math.floor(Math.random() * onlyLetters.length)     
+                    password += onlyLetters[randomCharacterIndex]
+                    } 
+                return password
+                }
+
+            if(btnState.symbols) {
+                for(let i = 0; i < num; i++) {
+                    let randomCharacterIndex = Math.floor(Math.random() * charactersWithoutSymbols.length)     
+                    password += charactersWithoutSymbols[randomCharacterIndex]
+                    } 
+                return password
             }
     
-        if(isNoSymbClicked) {
-            for(let i = 0; i < num; i++) {
-                let randomCharacterIndex = Math.floor(Math.random() * charactersWithoutSymbols.length)     
-                password += charactersWithoutSymbols[randomCharacterIndex]
-                } 
-            return password
-        }
+            if(btnState.numbers) {
+                for(let i = 0; i < num; i++) {
+                    let randomCharacterIndex = Math.floor(Math.random() * charactersWithoutNumbers.length)     
+                    password += charactersWithoutNumbers[randomCharacterIndex]
+                    } 
+                return password
+            }
     
-        if(isNoNumsClicked) {
             for(let i = 0; i < num; i++) {
-                let randomCharacterIndex = Math.floor(Math.random() * charactersWithoutNumbers.length)     
-                password += charactersWithoutNumbers[randomCharacterIndex]
+                let randomCharacterIndex = Math.floor(Math.random() * characters.length)     
+                password += characters[randomCharacterIndex]
                 } 
-            return password
-        }
-    
-        for(let i = 0; i < num; i++) {
-            let randomCharacterIndex = Math.floor(Math.random() * characters.length)     
-            password += characters[randomCharacterIndex]
-            } 
         return password
+
         } catch(err) {
             errorMessage.textContent = `Your input number is not valid because ${err}`
         }
     
     }
-    
-    
-    
-    
-
-    
-    
